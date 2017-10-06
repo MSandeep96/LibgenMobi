474cba3a2f761140cd52e10d3ef3012e7dd2f61b
Sandeep Reddy <msande1996@gmail.com>
2017-10-06 13:57:29 +0530

    First commit

---
+++ app/src/main/java/com/blowmymind/libgen/MainActivity.java
@@ -0,0 +1,21 @@
+package com.blowmymind.libgen;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
+    }
+
+}
